74465286f60ba71911ba7841858d67544d561680
vramakri2023 <startrekyoda2023@gmail.com>
2023-06-28 22:07:52 -0500

    udpates to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeat')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
