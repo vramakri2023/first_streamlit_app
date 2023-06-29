a35d86ec63ee475f0cd072e5e487ea12430e2734
vramakri2023 <startrekyoda2023@gmail.com>
2023-06-28 22:13:40 -0500

    Added Smoothie Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeat')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
-   
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')   
