b4a77b31c14c40fd77843ce3c2e34114f40e9780
vramakri2023 <startrekyoda2023@gmail.com>
2023-07-08 13:27:08 -0500

    code restructured

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 # created the mail python file
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeat')
@@ -9,7 +14,7 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')   
 
-import pandas
+
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -28,7 +33,7 @@ streamlit.header('Fruitvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # streamlit.text(fruityvice_response.json())
@@ -39,7 +44,9 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 # output to table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+# Don't run anything past here while we trouble shoot
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
