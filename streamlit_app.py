2f6149cd5c200370a3fa00bf894e88826645b67b
vramakri2023 <startrekyoda2023@gmail.com>
2023-07-08 12:37:56 -0500

    corrected type o

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 # output to table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector()
+import snowflake.connector
