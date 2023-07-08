31fda58d0b7897eeddcfc6f6ff1e1057e4e6d6b8
vramakri2023 <startrekyoda2023@gmail.com>
2023-07-08 13:36:14 -0500

    corrected type o

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ try:
         streamlit.error("Please select a fruit to get information")
     else:
         back_from_function= get_fruitvice_data(fruit_choice)
-        streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(back_from_function)
     
 except URLError as e:
     streamlit.error()
